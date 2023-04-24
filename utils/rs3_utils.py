f3bd1d762d2675d4d8a860cb26adea47841725d4
David Leys <davidjleys@gmail.com>
2023-04-24 13:16:01 +0700

    Updated function to divide 'skill xp' by 10 to return the correct result

--- utils/rs3_utils.py
+++ utils/rs3_utils.py
@@ -10,7 +10,10 @@ def get_player_stats(player_name):
         if 'error' in profile:
             return None
         else:
-            return profile['skillvalues']
+            skill_values = profile['skillvalues']
+            for skill in skill_values:
+                skill['xp'] = int(skill['xp'] / 10)  # Divide XP by 10
+            return skill_values
     except Exception as e:
         print(f"Error fetching player stats: {e}")
         return None
